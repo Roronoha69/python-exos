45ee0f87876e41283940c9a24be9afb6627dfc42
Roronoha69 <mbodje.justin@gmail.com>
2023-11-13 17:28:51 +0100

    custom test x)

--- tests/test_quadtree.py
+++ tests/test_quadtree.py
@@ -5,11 +5,11 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 from src import QuadTree, TkQuadTree
 
 def test_sample():
-    filename = "files/quadtree.txt"
+    filename = "../files/quadtree.txt"
     q = QuadTree.fromFile(filename)
-    assert q.depth == 4
+    assert QuadTree.depth(q) == 4
 
 def test_single():
-    filename = "files/quadtree_easy.txt"
+    filename = "../files/quadtree_easy.txt"
     q = QuadTree.fromFile(filename)
-    assert q.depth == 1
+    assert QuadTree.depth(q) == 1
